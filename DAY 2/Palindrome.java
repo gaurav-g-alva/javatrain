e2ec70a9d172af22cda35d7d244c3296ead77d98
Gaurav Alva <gauravgalva@gmail.com>
2025-02-11 14:53:21 +0530

    D2

---
+++ DAY 2/Palindrome.java
@@ -0,0 +1,30 @@
+//Write a Java Program to find the Palindrome of the number
+
+import java.util.Scanner;
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        int rev = reverse(num);
+        if (num == rev) {
+            System.out.println("Palindrome");
+        }
+        else
+        {
+            System.out.println("not palindrome");
+        }
+        sc.close();
+    }
+
+    public static int reverse(int num){
+        int Reverse=0;
+        while(num!=0){
+            int digit = num%10;
+            Reverse = Reverse*10+ digit;
+            num = num/10;
+        }
+        return Reverse;
+    
+    }
+}
