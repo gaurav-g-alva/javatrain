b03b4c3f7d27b096d37659fcecc96fb29d7e4f20
Gaurav Alva <gauravgalva@gmail.com>
2025-02-13 14:30:25 +0530

    D4

---
+++ DAY 4/Hcf.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Hcf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the Num 1 : ");
+            int num1 = sc.nextInt();
+            System.out.print("Enter the Num 2 : ");
+            int num2 = sc.nextInt();
+            sc.close();
+            int large = num1>num2?num1:num2;
+            for (int i = large/2; i >=1; i--) {
+                if (num1 % i ==0 && num2 % i ==0) {
+                    System.out.println(i+ " is HCF");
+                    break;
+                }
+            }
+            sc.close();
+    }
+    
+}
+
+    
