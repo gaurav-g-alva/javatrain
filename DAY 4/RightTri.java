b43592280b95d1d2e725f7398e8ac14b15d60099
Gaurav Alva <gauravgalva@gmail.com>
2025-02-13 10:17:15 +0530

    D4

---
+++ DAY 4/RightTri.java
@@ -0,0 +1,28 @@
+
+// Write a Java Program to print the * pattern in the Right Triangle.
+// *
+// *  *
+// *  *  *
+// *  *  *  *
+
+import java.util.Scanner;
+
+public class RightTri {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <=num; j++) {
+                if (i>=j) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    } 
+}
