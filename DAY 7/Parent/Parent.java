474b148b2ee8ab88bb77f67ccfdc18cf1652c168
Gaurav Alva <gauravgalva@gmail.com>
2025-02-17 10:53:42 +0530

    D7

--- DAY 7/Parent/Parent.java
+++ DAY 7/Parent/Parent.java
@@ -1,8 +1,13 @@
 public class Parent {
+    // static block 
     static {
         System.out.println("Parent static");
-    }{
-        System.out.println("Parent non static");
+    }
+    //non static block
+    {
+        System.out.println("Parent non static");  // the parent class constructor will be called by the super(); statement
+                                                    // the parent class construtor loads the non static member of parent class 
+                                                    // to the object created
         
     }
 }
--- DAY 7/Parent/child.java
+++ DAY 7/Parent/child.java
@@ -1,9 +1,11 @@
 
 public class child extends Parent {
-    static {
+    {
         System.out.println("Child  non static");
     }
     public static void main(String[] args) {
-        child c = new child();
+        child c = new child();           // the child class constructor will have the default super(); statement which
+                                        // will  call the parent class constructor , the parent consructor will load the 
+                                        // non static members to the object created in the child class 
     }
 }
