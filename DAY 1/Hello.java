fbcd3169f466d6a9b5d83a974f66a1437baacc2f
Gaurav Alva <gauravgalva@gmail.com>
2025-02-17 09:59:24 +0530

    D1 Update

---
+++ DAY 1/Hello.java
@@ -0,0 +1,14 @@
+// Print Hello 10 times using looping statements.
+public class Hello {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) //print hello 10 times
+        {
+            System.out.println(" Hello,World!!");
+        }
+
+        for (int i = 1; i <= 10; i++) //print 1-10 numbers
+        {
+            System.out.println(i);
+        }
+    }
+}
---
+++ DAY 1/div511.java
@@ -0,0 +1,18 @@
+// check if a number is divisible by 5 & 11
+import java.util.Scanner;
+public class div511 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a%5==0 && a%11==0)
+        {
+            System.out.println("yes " +a+ " is divisible by 5 & 11");
+        }
+        else
+        {
+            System.out.println("no "+a+" is not divisible by 5 & 11");
+        }
+        sc.close();
+	}
+}
+
